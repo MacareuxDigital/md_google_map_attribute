7b44d5ad339364a3753dcae6d5b98cc44af920d6
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-05-05 12:36:36 +0900

    Make applySortByNearest function static

--- src/Utility/QueryApplier.php
+++ src/Utility/QueryApplier.php
@@ -6,16 +6,17 @@ use Concrete\Core\Search\ItemList\Database\AttributedItemList;
 
 class QueryApplier
 {
-    public function applySortByNearest(AttributedItemList $list, string $attributeKeyHandle, float $latitude, float $longitude)
+    public static function applySortByNearest(AttributedItemList $list, string $attributeKeyHandle, float $latitude, float $longitude, string $order = 'ASC')
     {
         $latitude_column = 'ak_' . $attributeKeyHandle . '_latitude';
         $longitude_column = 'ak_' . $attributeKeyHandle . '_longitude';
         $list->getQueryObject()
-            ->addSelect("ST_Distance_Sphere(POINT($longitude_column, $latitude_column), POINT($longitude, $latitude)) AS distance")
+            ->addSelect("ST_Distance_Sphere(POINT({$longitude_column}, {$latitude_column}), POINT({$longitude}, {$latitude})) AS distance")
             ->andWhere($list->getQueryObject()->expr()->and(
                 $list->getQueryObject()->expr()->isNotNull($longitude_column),
                 $list->getQueryObject()->expr()->isNotNull($latitude_column)
             ))
-            ->orderBy('distance');
+            ->orderBy('distance')
+        ;
     }
 }

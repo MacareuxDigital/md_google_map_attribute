86b30f08f2b6bf411beebafeb7f517b4320c26bc
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-05-05 12:10:45 +0900

    Add QueryApplier

---
+++ src/Utility/QueryApplier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Macareux\Package\GoogleMapAttribute\Utility;
+
+use Concrete\Core\Search\ItemList\Database\AttributedItemList;
+
+class QueryApplier
+{
+    public function applySortByNearest(AttributedItemList $list, string $attributeKeyHandle, float $latitude, float $longitude)
+    {
+        $latitude_column = 'ak_' . $attributeKeyHandle . '_latitude';
+        $longitude_column = 'ak_' . $attributeKeyHandle . '_longitude';
+        $list->getQueryObject()
+            ->addSelect("ST_Distance_Sphere(POINT($longitude_column, $latitude_column), POINT($longitude, $latitude)) AS distance")
+            ->andWhere($list->getQueryObject()->expr()->and(
+                $list->getQueryObject()->expr()->isNotNull($longitude_column),
+                $list->getQueryObject()->expr()->isNotNull($latitude_column)
+            ))
+            ->orderBy('distance');
+    }
+}
